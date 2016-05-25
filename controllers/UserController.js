ce44d288a7a5dc27a1f6dc42ebeef9cf8c8bd176
NRose <niklasrose89@googlemal.com>
2016-05-25 15:44:59 +0200

    debugging getRandomQuests the random quest

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -117,8 +117,8 @@ function getOneRandomQuest(){
         
 
         var randomQuest = Math.floor(Math.random() * result) + 1; 
-        console.log("count: ", randomQuest);
-        var quest = Quest.find().limit(1).skip(randomQuest);
+        //console.log("count: ", randomQuest);
+        var quest = Quest.find().skip(randomQuest).limit(1).next();
         console.log("Quest: ", quest.ObjectId.valueOf());
         //return quest.ObjectId.valueOf();
       });
